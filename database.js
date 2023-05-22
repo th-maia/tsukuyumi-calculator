9126201bb750a44096ee29d186dd686f780446cd
thiago maia <thiago.maia@live.com>
2023-05-22 13:35:00 -0300

    primeiro comit

---
+++ database.js
@@ -0,0 +1,199 @@
+const boarlords = [
+  {
+    name: "Squeaker",
+    conquestValue: "15",
+    healthPoints: "10",
+    damageValue: "5",
+    quantity: "8",
+    marginLeft: "9.5%"
+  },
+  {
+    name: "Boarrior",
+    conquestValue: "15",
+    healthPoints: "10",
+    damageValue: "5",
+    quantity: "4",
+    marginLeft: "14.1%"
+  },
+  {
+    name: "Boar Maiden",
+    conquestValue: "15",
+    healthPoints: "15",
+    damageValue: "10",
+    quantity: "6",
+    marginLeft: "8%"
+  },
+  {
+    name: "Boarguard",
+    conquestValue: "50",
+    healthPoints: "20",
+    damageValue: "10",
+    quantity: "3",
+    marginLeft: "17%"
+  },
+  {
+    name: "Boarmaster",
+    conquestValue: "60",
+    healthPoints: "30",
+    damageValue: "15",
+    quantity: "1",
+    marginLeft: "9%"
+  },
+  {
+    name: "Boarmother",
+    conquestValue: "15",
+    healthPoints: "25",
+    damageValue: "10",
+    quantity: "1",
+    marginLeft: "9%"
+  },
+];
+
+const cybersamurai = [
+  {
+    name: "Attack Drone",
+    conquestValue: "5",
+    healthPoints: "5",
+    damageValue: "10",
+    quantity: "6"
+  },
+  {
+    name: "Shield Drone",
+    conquestValue: "5",
+    healthPoints: "15",
+    damageValue: "0",
+    quantity: "3"
+  },
+  {
+    name: "CyberSamurai 2.1",
+    conquestValue: "10",
+    healthPoints: "10",
+    damageValue: "10",
+    quantity: "4"
+  },
+  {
+    name: "CyberSamurai 2.2",
+    conquestValue: "10",
+    healthPoints: "10",
+    damageValue: "15",
+    quantity: "3"
+  },
+  {
+    name: "Cyberoid",
+    conquestValue: "20",
+    healthPoints: "20",
+    damageValue: "20",
+    quantity: "3"
+  },
+  {
+    name: "Shogun",
+    conquestValue: "25",
+    healthPoints: "25",
+    damageValue: "25",
+    quantity: "1"
+  },
+];
+
+const darkSeed = [
+  {
+    name: "Worker",
+    conquestValue: "5",
+    healthPoints: "5",
+    damageValue: "5",
+    quantity: "15"
+  },
+  {
+    name: "Breeder",
+    conquestValue: "5",
+    healthPoints: "5",
+    damageValue: "0",
+    quantity: "3"
+  },
+  {
+    name: "Wasp",
+    conquestValue: "5",
+    healthPoints: "5",
+    damageValue: "5",
+    quantity: "6"
+  },
+  {
+    name: "Warrior",
+    conquestValue: "10",
+    healthPoints: "10",
+    damageValue: "10",
+    quantity: "8"
+  },
+  {
+    name: "Conqueror",
+    conquestValue: "20",
+    healthPoints: "10",
+    damageValue: "0",
+    quantity: "3"
+  },
+  {
+    name: "Planter",
+    conquestValue: "15",
+    healthPoints: "15",
+    damageValue: "0",
+    quantity: "1"
+  },
+  {
+    name: "Stinger",
+    conquestValue: "15",
+    healthPoints: "15",
+    damageValue: "20",
+    quantity: "2"
+  }
+];
+
+const nomands = [
+  {
+    name: "Dog",
+    conquestValue: "5",
+    healthPoints: "5",
+    damageValue: "10",
+    quantity: "2"
+  },
+  {
+    name: "Private",
+    conquestValue: "10",
+    healthPoints: "10",
+    damageValue: "5",
+    quantity: "5"
+  },
+  {
+    name: "Stuff Sergeant",
+    conquestValue: "10",
+    healthPoints: "10",
+    damageValue: "0",
+    quantity: "2"
+  },
+  {
+    name: "Hunter",
+    conquestValue: "10",
+    healthPoints: "10",
+    damageValue: "10",
+    quantity: "2"
+  },
+  {
+    name: "Launcher",
+    conquestValue: "10",
+    healthPoints: "10",
+    damageValue: "15",
+    quantity: "2"
+  },
+  {
+    name: "Squad Leader",
+    conquestValue: "15",
+    healthPoints: "15",
+    damageValue: "15",
+    quantity: "1"
+  },
+  {
+    name: "Lt. Dan",
+    conquestValue: "15",
+    healthPoints: "15",
+    damageValue: "15",
+    quantity: "1"
+  }
+];
---
+++ script.js
@@ -0,0 +1,77 @@
+// import { boarlords } from './database/boarlords.js'; // estamos com problema de importação tendo que colocar no scrypt do arquivo principal o type="module" porem a função calculate fica sem funcionar.
+// https://www.youtube.com/watch?v=6Avdyl8YgWg&ab_channel=HighTechCursosF%C3%A1bricadeProgramador
+
+// deixar os bottoes com borda e radios  border-width: "2px" e border-radius: "8px"
+
+let total = [0,0,0];
+
+let selectedFaction = boarlords
+// changeButtons(boarlords)
+
+function changeFaction(faction, factionString) {
+	changeImage(factionString);
+  console.log(factionString)
+  selectedFaction = faction;
+  console.log(selectedFaction);
+ 	changeButtons(faction);
+  total = ["---","---","---"];
+  writeCalculatedResult()
+}
+
+function changeImage(factionString) {
+	const image = document.getElementById("factionImage");
+	const location = document.createAttribute("src");
+	location.value = `./images/${factionString}.png`;
+
+	image.setAttributeNode(location);
+}
+
+function changeButtons(faction) {
+  const father = document.getElementById("unitsButtons")
+  father.innerHTML = "";
+
+  for (let i = 0; i < faction.length; i+=1 ) {
+		let selectInput = document.createElement("select");
+    selectInput.setAttribute("onchange","calculate()");
+    father.appendChild(selectInput);
+
+    for (let e = 0; e <= faction[i].quantity; e+=1 ) {
+      let optionNumber = document.createElement("option");
+      optionNumber.value = e
+      optionNumber.innerHTML = e
+      selectInput.appendChild(optionNumber);
+    }
+	}
+}
+
+function calculate() {
+	const childrens = document.getElementById("unitsButtons").children
+	const numberOfButtons = childrens.length;
+	for (let i = 0; i < numberOfButtons; i+=1 ) {
+		total[0] += childrens[i].value * selectedFaction[i].conquestValue;
+	}
+
+	for (let i = 0; i < numberOfButtons; i+=1 ) {
+		total[1] += childrens[i].value * selectedFaction[i].healthPoints;
+	}
+
+  for (let i = 0; i < numberOfButtons; i+=1 ) {
+		total[2] += childrens[i].value * selectedFaction[i].damageValue;
+	}
+  writeCalculatedResult()
+}
+
+function writeCalculatedResult() {
+  const result = document.getElementById("total").children
+  for (let i = 0; i < 3; i+=1 ) {
+		result[1+i+i].innerText = total[i]
+	}
+  total = [0,0,0]
+}
+
+function saveTotal() {
+  const button = document.getElementById(event.target.id)
+  const total = document.getElementById("total").innerText;
+  button.innerHTML = total
+  
+}
